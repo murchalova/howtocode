d97de84fc59ea8473424a04a23aa8e51ff7dac67
murchalova <ukr.inko@gmail.com>
2017-02-16 20:31:29 +0200

    Простые числа с break.

--- primes.py
+++ primes.py
@@ -1,7 +1,7 @@
 primes = list(range(200))
-
 i = 2
-while i < 200:
+n = 200
+while True:
     if primes[i] != 0:
         j = i * 2
         while j < 200:
@@ -9,7 +9,8 @@ while i < 200:
             p = j + i
             j = p
     i = i + 1
-
+    if i == n:
+        break
 for s in primes:
     if primes[s] != 0:
         print(primes[s], end=' ')
